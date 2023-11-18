12ddeef900c4d888c6ef801fb75607035fc8f747
SilverioPacheco <jefecitochung18@gmail.com>
2023-11-18 17:41:32 -0400

    post

--- my-app/src/app/pages/home/home.component.ts
+++ my-app/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LenguajesService } from 'src/app/services/lenguajes.service';
 import { UsuariosService } from 'src/app/services/usuarios.service'
+import { LanguagesService } from 'src/app/services/languages.service';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,11 @@ import { UsuariosService } from 'src/app/services/usuarios.service'
 export class HomeComponent implements OnInit {
   dataUsers: any = [];
   dataLanguages: any = [];
+  name:string = "Python";
+  abrev:string = "Py";
+  dataSource:any = [];
 
-  constructor(private usersServices: UsuariosService, private lenguajesServices: LenguajesService){}
-
+  constructor(private usersServices: UsuariosService, private lenguajesServices: LenguajesService, private language: LanguagesService ){}
   
   ngOnInit()
   {
@@ -24,5 +27,59 @@ export class HomeComponent implements OnInit {
       let arrayLenguajes = [data];
       this.dataLanguages = arrayLenguajes;
     });
+
+    this.language.getListLanguges().subscribe( (data) => {
+      for(var key in data)
+      {
+        var row = {id:key, abrev: data[key].abrev, name: data[key].name}
+        this.dataSource.push(row)
+      }
+      console.log(this.dataSource)
+    } )
   }
+  save()
+{
+  let body = 
+  {
+    name: this.name,
+    abrev: this.abrev
+  }
+  this.language.postLanguage(body).subscribe( (data) => {
+    if(data!=null)
+    {
+      window.location.reload();
+    }
+  })
 }
+borrar(id:string){
+  let aux = confirm("Esta Seguro de Borrar")
+  if(!aux) return
+  this.language.deleteLanguage(id).subscribe( (data) => {
+    if(data==null)
+    {
+      window.location.reload();
+    }
+  })
+}
+
+actualizar(id:string){
+  let aux = confirm("Esta Seguro de Actualizar")
+  let body = 
+  {
+    abrev: "test Upt abrev",
+    name:  "test Upt name"
+  }    
+  if(!aux) return
+  this.language.updateLanguage(id, body).subscribe( (data) => {
+    if(data!=null)
+    {
+      window.location.reload();
+    }
+  })
+}
+}
+
+
+ 
+
+
--- my-app/src/app/services/lenguajes.service.ts
+++ my-app/src/app/services/lenguajes.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LenguajesService {
 
-  url: string = "https://apirest-519d9-default-rtdb.firebaseio.com/v1/topList.json"
+    url: string = "https://apirest-519d9-default-rtdb.firebaseio.com/v1/topList.json"
  
   constructor(private http: HttpClient) { }
 
@@ -16,4 +16,3 @@ export class LenguajesService {
     return this.http.get(this.url)
   }
 }
-
